add333ea0b365fe1260a1dc098b2200de4e8b43c
Aviral <uncleprosk@gmail.com>
2026-07-10 02:30:02 +0530

    add: basic structure of page 1

--- src/components/pages/Page1.jsx
+++ src/components/pages/Page1.jsx
@@ -1,11 +1,16 @@
 import TextPressure from "../ext/TextPressure";
 import LineWaves from "../ext/LineWaves"
+import ASCIIText from '../ext/AsciiText';
 
 export default function Page1() {
 
     return (
-        <div className="w-[800px] h-[500px] bg-red-500 flex flex-col justify-center items-center rounded-3xl">
-            <h1 className="text-6xl font-bold text-white whitespace-nowrap">unclePRO</h1>
+        <div className="w-[800px] h-[500px] flex flex-col justify-center items-center rounded-3xl">
+            <ASCIIText
+            text="unclePRO"
+            enableWaves
+            asciiFontSize={10}
+            />
         </div>
     )
 }
