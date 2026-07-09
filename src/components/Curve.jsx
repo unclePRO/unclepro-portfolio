80e3d3f0ae5ed94fc18ed5d2cb674ecaf45847e4
Aviral <uncleprosk@gmail.com>
2026-07-10 00:42:14 +0530

    update: new path for 4 pages

--- src/components/Curve.jsx
+++ src/components/Curve.jsx
@@ -1,9 +1,9 @@
 import * as THREE from 'three'
 
 export const curve = new THREE.CatmullRomCurve3([
-  new THREE.Vector3(0, 0, 0),
-  new THREE.Vector3(2, 1, -20),
-  new THREE.Vector3(-3, -2, -45),
-  new THREE.Vector3(0, 0, -70),
-  new THREE.Vector3(0, -1, -100),
+  new THREE.Vector3(0, 0, 0),        
+  new THREE.Vector3(30, -5, -250),   
+  new THREE.Vector3(-40, 20, -500),  
+  new THREE.Vector3(20, 30, -750),  
+  new THREE.Vector3(0, 0, -1000)
 ], false, 'catmullrom', 0.5)
