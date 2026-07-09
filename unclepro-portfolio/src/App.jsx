2642618b93a56589985cda9469bbf3be3f8584a2
Aviral <uncleprosk@gmail.com>
2026-07-09 21:49:13 +0530

    new: react setup and THREEjs integration

--- unclepro-portfolio/src/App.jsx
+++ unclepro-portfolio/src/App.jsx
@@ -3,10 +3,15 @@ import Scene from './components/Scene'
 
 export default function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    // We are using raw style={{}} here to force the browser to obey.
+    // This absolutely padlocks the container to the screen.
+    <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', overflow: 'hidden', backgroundColor: '#050505' }}>
+      
       <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
+        {/* Make sure pages={2} is inside this file! */}
         <Scene />
       </Canvas>
+
     </div>
   )
 }
