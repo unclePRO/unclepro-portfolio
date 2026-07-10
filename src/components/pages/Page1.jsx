1e0f8ffb02ec4151036f969167b4b647af556b1b
Aviral <uncleprosk@gmail.com>
2026-07-11 01:26:46 +0530

    update: added a view button to view scenary

--- src/components/pages/Page1.jsx
+++ src/components/pages/Page1.jsx
@@ -1,6 +1,9 @@
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { SiLeetcode } from 'react-icons/si';
 import HackerShuffle from '../ext/HackerShuffle';
+import DrawOutlineButton from '../ext/DrawOutlineButton'
+import { useState } from 'react';
+
 const links = [
     { icon: FaGithub, href: "https://github.com/unclepro", label: "Github" },
     { icon: SiLeetcode, href: "https://leetcode.com/u/unclepro", label: "LeetCode" },
@@ -8,20 +11,42 @@ const links = [
 ];
 
 export default function Page1() {
-    return (
-        <div className="relative w-[65vw] h-[65vh] bg-black/90 flex flex-col justify-between items-center rounded-3xl pointer-events-auto p-8 border border-cyan-500/20 backdrop-blur-md overflow-hidden">
+    const [ hidePage, setHidePage ] = useState(false);
 
-            <p className='font-mono text-xs tracking-widest text-cyan-500/50 uppercase z-10'>
-                hi
+    if (hidePage) {
+        return (
+            <div className='relative w-[70vw] h-[70vh] flex flex-col justify-between items-center rounded-3xl pointer-events-auto p-8 border border-cyan-500/20 backdrop-blur-md overflow-hidden'>
+                <div className="absolute top-10 left-10">
+                    <DrawOutlineButton onClick={() => setHidePage(false)}>close</DrawOutlineButton>
+                </div>
+            </div>
+        )
+    }
+    return (
+        <div className="relative w-[70vw] h-[70vh] bg-black/70 flex flex-col justify-between items-center rounded-3xl pointer-events-auto p-8 border border-cyan-500/20 backdrop-blur-md overflow-hidden">
+
+            <div className="absolute top-10 left-10">
+                <DrawOutlineButton onClick={() => setHidePage(true)}>view</DrawOutlineButton>
+            </div>
+
+            <p className='text-xs tracking-widest text-cyan-500/50'>
+                Youkoso
             </p>
 
             {/* Intro */}
-            <div className="relative w-full flex-1 flex justify-center items-center my-4">
+            <div className="relative w-full flex-col flex justify-center items-center my-4">
                 <HackerShuffle 
                     text="unclePRO" 
                     duration={0.5} 
                     triggerOnHover={true}
+                    className='hover:scale-150 duration-10000'
                 />
+                
+                <div className='w-full max-w-2xl text-center z-10 px-4'>
+                    <p className='text-sm md:text-base tracking-wide opacity-50 hover:opacity-100 hover:scale-102 duration-500 text-cyan-500 leading-relaxed'>
+                        Yare Yare daze. The web needs better architecture.
+                    </p>
+                </div>
             </div>
 
             {/* Bottom Nav Links */}
