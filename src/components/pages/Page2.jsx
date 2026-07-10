24b6ec62428b3e36c64dd7f7dc52bf98d4916eef
Aviral <uncleprosk@gmail.com>
2026-07-11 00:18:06 +0530

    update: basic structure updated

--- src/components/pages/Page2.jsx
+++ src/components/pages/Page2.jsx
@@ -1,8 +1,26 @@
+import { GitHubCalendar } from "react-github-calendar";
+import LeetCodeCalendar from "../ext/LeetCodeCalender";
+
 export default function Page2() {
 
     return (
-        <div className="w-[800px] h-[500px] bg-red-500 flex flex-col justify-center items-center rounded-3xl">
-            <h1 className="text-6xl font-bold text-white whitespace-nowrap">2nd page fr</h1>
+        <div className="relative w-[65vw] h-[65vh] bg-black/90 flex flex-col justify-between items-center rounded-3xl pointer-events-auto p-8 border border-cyan-500/20 backdrop-blur-md overflow-hidden">
+            <div className="mt-10 pt-4">
+                <GitHubCalendar 
+                    username="unclepro" 
+                    colorScheme="dark"
+                    theme={{
+                        dark: ['#14191f', '#0891b2', '#06b6d4', '#22d3ee', '#67e8f9']
+                    }}
+                    labels={{
+                        totalCount: '{{count}} contributions made in the past year',
+                    }}
+                    className="text-white !overflow-visible"
+                />
+            </div>
+            <div className="mt-10 pt-4 [&_article]:overflow-visible [&_svg]:overflow-visible">
+                <LeetCodeCalendar username="unclepro" />
+            </div>
         </div>
     )
 }
--- src/components/pages/Page4.jsx
+++ src/components/pages/Page4.jsx
@@ -1,7 +1,7 @@
 export default function Page4() {
 
     return (
-        <div className="w-[800px] h-[500px] bg-red-500 flex flex-col justify-center items-center rounded-3xl">
+        <div className="relative w-[65vw] h-[65vh] bg-black/90 flex flex-col justify-between items-center rounded-3xl pointer-events-auto p-8 border border-cyan-500/20 backdrop-blur-md overflow-hidden">
             <h1 className="text-6xl font-bold text-white whitespace-nowrap">4nd page fr</h1>
         </div>
     )
