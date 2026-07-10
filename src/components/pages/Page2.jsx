d389d32e9464fb9e7600d43a67cbf376fee0c33b
Aviral <uncleprosk@gmail.com>
2026-07-11 01:27:42 +0530

    update: github and leetcode graphs

--- src/components/pages/Page2.jsx
+++ src/components/pages/Page2.jsx
@@ -4,21 +4,23 @@ import LeetCodeCalendar from "../ext/LeetCodeCalender";
 export default function Page2() {
 
     return (
-        <div className="relative w-[65vw] h-[65vh] bg-black/90 flex flex-col justify-between items-center rounded-3xl pointer-events-auto p-8 border border-cyan-500/20 backdrop-blur-md overflow-hidden">
-            <div className="mt-10 pt-4">
+        <div className="relative w-[70vw] h-[70vh] bg-black/80 flex flex-col justify-between rounded items-center pointer-events-auto p-8 border border-cyan-500/20 backdrop-blur-md overflow-hidden">
+            <h1 className="text-4xl font-bold text-white whitespace-nowrap">Github</h1>
+            <div className="[&_article]:!overflow-visible [&_svg]:!overflow-visible [&_text]:!translate-y-[6px]">
                 <GitHubCalendar 
                     username="unclepro" 
                     colorScheme="dark"
                     theme={{
-                        dark: ['#14191f', '#0891b2', '#06b6d4', '#22d3ee', '#67e8f9']
+                        dark: ['#161b22', '#0e4429', '#006d32', '#26a641', '#39d353']
                     }}
                     labels={{
                         totalCount: '{{count}} contributions made in the past year',
                     }}
-                    className="text-white !overflow-visible"
+                    className="text-white"
                 />
             </div>
-            <div className="mt-10 pt-4 [&_article]:overflow-visible [&_svg]:overflow-visible">
+            <h1 className="text-4xl font-bold text-white whitespace-nowrap">Leetcode</h1>
+            <div className="[&_article]:overflow-visible [&_svg]:overflow-visible [&_text]:!translate-y-[6px]">
                 <LeetCodeCalendar username="unclepro" />
             </div>
         </div>
